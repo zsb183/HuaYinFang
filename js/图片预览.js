72ea08c1a4c2de1d5758f7e83890f56e31e152c2
zhai <lmy130852@163.com>
2021-10-29 17:37:32 +0800

    项目第二天

--- js/图片预览.js
+++ js/图片预览.js
@@ -1,7 +1,7 @@
-$('.img').click(function() {
+$('.main_ul>li').click(function() {
 
     //获取图片路径
-    var imgsrc = $(this).attr("src");
+    var imgsrc = $(".img").attr("src");
     // console.log(imgsrc);
     var opacityBottom = '<div class="opacityBottom" style = "display:none"><img class="bigImg changeImg" src="' + imgsrc + '"></div>';
     $(document.body).append(opacityBottom);
@@ -21,15 +21,16 @@ $('.img').click(function() {
 function toBigImg() {
     $(".opacityBottom").addClass("opacityBottom"); //添加遮罩层
     $(".opacityBottom").show();
-    $(".bigImg").addClass("bigImg"); //添加图片样式
-    $(".opacityBottom").click(function() { //点击关闭
-        // $("html,body").removeClass("none-scroll");
-        $(".opacityBottom").remove();
-    });
-    // $(".opacityBottom").on("tap", function(e) {
-    //     $("html,body").removeClass("none-scroll");
+    // $(".bigImg").addClass("bigImg"); //添加图片样式
+    // $(".opacityBottom").click(function() { //点击关闭
+    //     // $("html,body").removeClass("none-scroll");
     //     $(".opacityBottom").remove();
-    // })
+    // });
+    $(".opacityBottom").on("tap", function(e) {
+        $("html,body").removeClass("none-scroll");
+        $(".opacityBottom").remove();
+    })
+
 
 
 }
